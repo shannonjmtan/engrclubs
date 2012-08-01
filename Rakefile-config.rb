11ef84f82f51e89a679d7d11c3fb43aa25e0d0f4
Eric Bollens <ebollens@ucla.edu>
2012-08-01 15:24:36 -0700

    Add custom SASS path to WebBlocks configuration [#1]

--- Rakefile-config.rb
+++ Rakefile-config.rb
@@ -2,4 +2,9 @@ require 'pathname'
 
 rootdir = File.dirname(Pathname.new(__FILE__).realpath)
 
-WebBlocks.config[:build][:dir] = "#{rootdir}/www/assets"
+# TODO: should be www/assets instead of www/blocks but can't be until custom JS,
+# images and HTML are supported or otherwise this ends up overwriting other 
+# assets that the developer places within www/assets
+WebBlocks.config[:build][:dir] = "#{rootdir}/www/assets/blocks"
+
+WebBlocks.config[:src][:sass] = "#{rootdir}/src/sass"
