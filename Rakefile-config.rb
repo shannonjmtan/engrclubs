37f0b2eb94fc95bed757d8a24acf9797b3ee7238
Eric Bollens <ebollens@ucla.edu>
2012-08-16 01:00:12 -0700

    Update config for WebBlocks upgrade

--- Rakefile-config.rb
+++ Rakefile-config.rb
@@ -1,32 +1,31 @@
-# Determine absolute pathing for use in the build settings
-
 require 'pathname'
 rootdir = File.dirname(Pathname.new(__FILE__).realpath)
 
-
-
-# The directory where WebBlocks is built
 #
-# TODO: should be www/assets instead of www/blocks but can't be until custom JS,
-# images and HTML are supported or otherwise this ends up overwriting other 
-# assets that the developer places within www/assets
+# BASIC WEBBLOCKS CONFIGURATION SETTINGS
+#
 
+# The directory into which WebBlocks is built
 WebBlocks.config[:build][:dir] = "#{rootdir}/www/assets/blocks"
 
+# The directory where all
+WebBlocks.config[:src][:dir] = "#{rootdir}/src"
 
-
-# The directory where the SASS files site.scss and site-ie.scss are pulled from
-# for a WebBlocks build
-
-WebBlocks.config[:src][:sass] = "#{rootdir}/src/sass"
-
-
-
-# The Bootstrap plugins that should be packaged in the WebBlocks build
-
+# Bootstrap plugins that should be included in the build
 WebBlocks.config[:package][:bootstrap][:scripts] = [
     'dropdown',
     'button',
     'collapse',
     'alert'
   ];
+
+
+#
+# ADVANCED WEBBLOCKS CONFIGURATION SETTINGS (change at your own risk!)
+#
+
+# Location of WebBlocks core components (config.rb, definitions, core adapter)
+WebBlocks.config[:src][:core][:dir] = "#{rootdir}/package/WebBlocks/src/core"
+
+# Location of WebBlocks adapters
+WebBlocks.config[:src][:adapters][:dir] = "#{rootdir}/package/WebBlocks/src/adapter"
