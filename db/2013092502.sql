61132f64a574cc8769f8b4675bccd812f1081fe1
shannontan94 <shannontan@oit.ucla.edu>
2013-09-25 10:36:18 -0700

    Change for ASCE

---
+++ db/2013092502.sql
@@ -0,0 +1,4 @@
+UPDATE  `engrclubs`.`clubs` SET  `upcoming_events` =  '<ul>
+                    <li>ASCE Open House - Wednesday of 2nd Week</li>
+                    <li>ASCE Fall Career Fair - November 4th</li>
+                </ul>' WHERE  `clubs`.`id` =2;
